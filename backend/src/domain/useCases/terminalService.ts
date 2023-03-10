ec4a6d7667399a502341270ce7b30bacf4ceb230
André Stanlley <andre.v@lyncas.net>
2023-03-10 09:46:39 -0300

    fix: bug

--- backend/src/domain/useCases/terminalService.ts
+++ backend/src/domain/useCases/terminalService.ts
@@ -36,24 +36,19 @@ export class TerminalService implements ITerminalService {
 		const download: IMediaProps[] =
 			medias?.filter(
 				(mediaLocal: IMediaProps) =>
-					!localVideos?.find(
-						(media: IMediaProps) =>
-							media.filename === mediaLocal.filename &&
-							mediaLocal.expiresIn >= new Date()
-					)
+					!localVideos.map((md) => md.filename).includes(mediaLocal.filename) &&
+					mediaLocal.expiresIn >= new Date()
 			) ?? [];
 
 		const remove: IMediaProps[] =
 			localVideos?.filter((mediaLocal: IMediaProps) => {
 				const media = medias?.find((md) => md.filename === mediaLocal.filename);
 
-				if (
+				return (
 					!(media?.size === mediaLocal.size) ||
 					!(media.expiresIn >= new Date()) ||
 					!medias?.map((m) => m.filename).includes(mediaLocal.filename)
-				) {
-					return true;
-				}
+				);
 			}) ?? [];
 
 		return {
